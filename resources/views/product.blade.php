c055e5e04a3433115ceff0d69a5bbb269ce6caaf
peekinternational <peekinternational@gmail.com>
2018-12-10 19:14:07 +0500

    navsssssss

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -70,11 +70,12 @@
                             @endif
                             <span class="price">
                                 <span>
-                                @if($product->offer)
+                                    @if($product->offer)
                                 <span class="amount" style="color: gray !important; font-size: 26px;">&euro;<strike>{{ $product->price }}</strike></span>
                                <span class="amount" style="color: gray !important; font-size: 26px;">&euro;{{ $product->price }}</span>
                                 @else
                                     <span class="amount" style="color: gray !important; font-size: 26px;">&euro;{{ $product->price }}</span>
+                               @endif
                                 </span>
                             </span>
                             <div class="quantity-btn">
