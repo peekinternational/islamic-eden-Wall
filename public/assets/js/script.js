4c0464ec68037740fe30d8e0e77eadd113f37b5d
peekinternational <peekinternational@gmail.com>
2019-01-15 23:29:23 -0800

    updateddddddddddddddd

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1524,7 +1524,7 @@ jQuery( function() {
 		    var scroll = $(window).scrollTop();
 		    // console.log(scroll);
 		    if (scroll <=100) {
-		        $(".subnav-content").css("top", "10%");
+		        $(".subnav-content").css("top", "15%");
 		    }else if(scroll >100) {
 		    	$(".subnav-content").css("top", "59px");
 		    }else {
