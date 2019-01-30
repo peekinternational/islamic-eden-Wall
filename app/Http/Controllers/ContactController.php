51d866dd019482c147e5d3315749ff8b12e739b1
peekinternational <peekinternational@gmail.com>
2019-01-30 14:38:39 -0800

    updated

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -25,6 +25,27 @@ class ContactController extends Controller
     {
         //
     }
+     public function contactus(Request $request)
+    {
+
+             $this->validate($request,[
+            'name' => 'required',
+            'email' =>'required',
+            'message' =>'required'
+        ]);
+                         // $inputs['payer_email']='rizwanahmadabbasi05@gmail.com';
+                         $inputs=$request->all();
+                         $toemail ='rizwanahmadabbasi05@gmail.com';
+                        Mail::send('emails.contactus',['contact' =>$inputs],
+                        function ($message) use ($toemail)
+                        {
+                            $message->subject('Islamic Wall - Order Received');
+                            $message->from('nabeelirbab@gmail.com', 'Islamic Wall Design');
+                            $message->to($toemail);
+                         });
+                         return back();
+
+    }
 
     /**
      * Store a newly created resource in storage.
